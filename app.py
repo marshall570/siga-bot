35e6eeea1a6428f1a8bf3dda2643def421e69548
Gustavo <marshall@arch.localdomain>
2021-10-18 10:21:41 -0300

    Updates: Find BY & ChromeDriver

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import os
 from logging import error
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 
 try:
     # CARREGAR .ENV DO ALUNO
@@ -15,25 +16,25 @@ try:
     login_button = '.Button'
     span_notas = '#ygtvlabelel10Span'
 
-    # CRIAR WEBDRIVER
+    # CRIAR WEBDRIVER CHROMIUM
     option = Options()
     option.headless = True
-    driver = webdriver.Firefox(options=option)
+    driver = webdriver.Chrome(options=option)
     driver.implicitly_wait(2)
 
     # OBTER NOTAS
     driver.get('https://siga.cps.sp.gov.br/aluno/login.aspx')
 
-    driver.find_element_by_css_selector(user_input).send_keys(os.getenv('USER-LOGIN'))
-    driver.find_element_by_css_selector(pswd_input).send_keys(os.getenv('USER-PSWD'))
-    driver.find_element_by_css_selector(login_button).click()
+    driver.find_element(By.CSS_SELECTOR, user_input).send_keys(os.getenv('USER-LOGIN'))
+    driver.find_element(By.CSS_SELECTOR, pswd_input).send_keys(os.getenv('USER-PSWD'))
+    driver.find_element(By.CSS_SELECTOR, login_button).click()
     
-    driver.find_element_by_css_selector(span_notas).click()
+    driver.find_element(By.CSS_SELECTOR, span_notas).click()
 
     for i in range(1, 9):
-        nome = driver.find_element_by_id(f'span_vACD_DISCIPLINANOME_000{i}').get_attribute('textContent')
+        nome = driver.find_element(By.ID, f'span_vACD_DISCIPLINANOME_000{i}').get_attribute('textContent')
 
-        nota = driver.find_element_by_id(f'span_vACD_ALUNOHISTORICOITEMMEDIAFINAL_000{i}').get_attribute('textContent')
+        nota = driver.find_element(By.ID, f'span_vACD_ALUNOHISTORICOITEMMEDIAFINAL_000{i}').get_attribute('textContent')
 
         print(f'{nota} - {nome}')
 
@@ -41,4 +42,3 @@ try:
 
 except Exception as e:
     error(e)
-    driver.quit()
