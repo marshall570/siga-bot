8598517bc203d7acaa522106faa7459676c79466
Gustavo Fernandes <gustavosergio2001@gmail.com>
2021-12-14 15:03:21 -0300

    Drivers Updates

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import os
 from logging import error
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 
 try:
-    # CARREGAR .ENV DO ALUNO
+    # CARREGAR INFO DO ALUNO COM O .ENV
     load_dotenv()
+    user = os.getenv('USER-LOGIN')
+    pswd = os.getenv('USER-PSWD')
 
     # SELECTORS NECESSÁRIOS
     user_input = '#vSIS_USUARIOID'
@@ -17,24 +20,36 @@ try:
     span_notas = '#ygtvlabelel10Span'
 
     # CRIAR WEBDRIVER CHROMIUM
+    # option = Options()
+    # option.headless = True
+    # driver = webdriver.Chrome(options=option)
+    # driver.implicitly_wait(2)
+
+    # CRIAR WEBDRIVER FIREFOX
     option = Options()
     option.headless = True
-    driver = webdriver.Chrome(options=option)
+    driver = webdriver.Firefox(options=option)
     driver.implicitly_wait(2)
 
     # OBTER NOTAS
     driver.get('https://siga.cps.sp.gov.br/aluno/login.aspx')
 
-    driver.find_element(By.CSS_SELECTOR, user_input).send_keys(os.getenv('USER-LOGIN'))
-    driver.find_element(By.CSS_SELECTOR, pswd_input).send_keys(os.getenv('USER-PSWD'))
+    driver.find_element(By.CSS_SELECTOR, user_input).send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, pswd_input).send_keys(pswd)
     driver.find_element(By.CSS_SELECTOR, login_button).click()
-    
+
     driver.find_element(By.CSS_SELECTOR, span_notas).click()
 
     for i in range(1, 9):
-        nome = driver.find_element(By.ID, f'span_vACD_DISCIPLINANOME_000{i}').get_attribute('textContent')
+        nome = driver.find_element(
+            By.ID,
+            f'span_vACD_DISCIPLINANOME_000{i}'
+        ).get_attribute('textContent')
 
-        nota = driver.find_element(By.ID, f'span_vACD_ALUNOHISTORICOITEMMEDIAFINAL_000{i}').get_attribute('textContent')
+        nota = driver.find_element(
+            By.ID,
+            f'span_vACD_ALUNOHISTORICOITEMMEDIAFINAL_000{i}'
+        ).get_attribute('textContent')
 
         print(f'{nota} - {nome}')
 
