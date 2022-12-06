870b3db5e64cd56a3f328b4fdda58ed71d15e03b
Gustavo Fernandes <gustavosergio2001@gmail.com>
2022-12-06 12:00:57 -0300

    delete notify

--- app.py
+++ app.py
@@ -1,56 +1,31 @@
 import os
-from plyer import notification
-from logging import error
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-# from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 try:
-    # CARREGAR INFO DO ALUNO COM O .ENV
     load_dotenv()
     user = os.getenv('USER-LOGIN')
     pswd = os.getenv('USER-PSWD')
-
-    grades = ''
     
-    # SELECTORS NECESSÁRIOS
     user_input = '#vSIS_USUARIOID'
     pswd_input = '#vSIS_USUARIOSENHA'
     login_button = '.Button'
     span_notas = '#ygtvlabelel10Span'
 
-    # CRIAR WEBDRIVER FIREFOX
-    firefox_options = FirefoxOptions()
+    firefox_options = Options()
     firefox_options.headless = True
     driver = webdriver.Firefox(options=firefox_options)
-    
-    # CRIAR WEBDRIVER CHROME
-    # chrome_options = ChromeOptions()
-    # chrome_options.headless = True
-    # driver = webdriver.Chrome(options=chrome_options)
-    
     driver.implicitly_wait(2)
-    
-    notification.notify(
-        app_name = 'SIGA',
-        title = 'EXECUTANDO',
-        message =  'Logando no sistema...',
-        timeout = 3
-    )
 
-    # OBTER NOTAS
     driver.get('https://siga.cps.sp.gov.br/aluno/login.aspx')
-
     driver.find_element(By.CSS_SELECTOR, user_input).send_keys(user)
     driver.find_element(By.CSS_SELECTOR, pswd_input).send_keys(pswd)
     driver.find_element(By.CSS_SELECTOR, login_button).click()
-
     driver.find_element(By.CSS_SELECTOR, span_notas).click()
-
     
-    for i in range(1, 10):
+    for i in range(1, 11):
         if i < 10:
             nome = driver.find_element(
                 By.ID,
@@ -72,22 +47,9 @@ try:
                 f'span_vACD_ALUNOHISTORICOITEMMEDIAFINAL_00{i}'
             ).get_attribute('textContent')
 
-        # print(f'{nota} - {nome}')
-        grades += f'{nota} - {nome}\n'
-
-    notification.notify(
-        app_name = 'SIGA',
-        title = 'NOTAS ATUAIS',
-        message =  grades,
-        timeout = 10
-    )
+        print(f'{nota} - {nome}')
 
     driver.quit()
 
 except Exception as e:
-    notification.notify(
-        app_name = 'SIGA',
-        title = 'ERRO',
-        message = e,
-        timeout = 10
-    )
+    print(e)
